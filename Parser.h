a631552608a91e0df6c21ff5083cb6c9be4be6cc
a2ys <oreus.aks@gmail.com>
2026-01-11 00:03:19 +0530

    chore: expose all variables and functions

--- Parser.h
+++ Parser.h
@@ -4,7 +4,12 @@
 #include "AST.h"
 #include <memory>
 
-// We will be using operator precedence parsing here.
+// Expose all variables and functions to other files
+extern int CurTok;
+int getNextToken();
+void InitializePrecedence();
+
+std::unique_ptr<ExprAST> ParseExpression();
 std::unique_ptr<ExprAST> Parse();
 
 #endif
